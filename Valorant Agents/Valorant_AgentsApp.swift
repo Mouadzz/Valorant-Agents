729ba15fd81735c82ea2b2c27852a813a54f2889
Mouadz <mlasrite@student.1337.ma>
2021-12-08 14:13:19 +0100

    first commit

---
+++ Valorant Agents/Valorant_AgentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valorant_AgentsApp.swift
+//  Valorant Agents
+//
+//  Created by Mouaad Lasritene on 8/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Valorant_AgentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
