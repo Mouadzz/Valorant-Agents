87e578d6738bc2d71cef1f78d6ddbae3e7ec4557
Mouadz <mlasrite@student.1337.ma>
2021-12-10 11:12:14 +0100

    Card with 3d effect + responsive

--- Valorant Agents/Views/AgentsView.swift
+++ Valorant Agents/Views/AgentsView.swift
@@ -11,26 +11,32 @@ struct AgentsView: View {
     
     @EnvironmentObject var valAgents: ValAgents
     
-    var colors: [Color] = [.blue, .green, .orange, .red, .gray, .pink, .yellow]
-    
+    let cardWidth:Double = UIScreen.screenWidth  / 1.8
+    var HPadding:Double =  ((UIScreen.screenWidth  - (UIScreen.screenWidth  / 1.8)) / 2)
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: UIScreen.screenWidth * 0.05){
-                ForEach (0..<self.valAgents.agents.data.count, id:\.self) { index in
+        ScrollView(.horizontal) {
+            HStack(spacing: UIScreen.screenWidth * 0.01){
+                ForEach (self.valAgents.agents.data.indices, id:\.self) { index in
                     let agent = self.valAgents.agents.data[index] as Datum
-                    GeometryReader { geometry in
-                        ZStack{
-                            Color.purple.brightness(-0.5)
-                            if agent.fullPortrait != nil{
-                                ImageLoadingView(url: agent.fullPortrait!)
-                            }
-                        }.cornerRadius(20)
-                            .rotation3DEffect(Angle(degrees:(Double(geometry.frame(in: .global).minX) - (UIScreen.screenWidth  / 3.6) + UIScreen.screenWidth * 0.05) / -15), axis: (x: 0, y: 1.0, z: 0))
-                    }.frame(width: UIScreen.screenWidth  / 1.8)
+                    if agent.fullPortrait != nil{
+                        GeometryReader { geometry in
+                            ZStack{
+                                Color.purple.brightness(-0.5)
+                                VStack{
+                                    Text("\(geometry.frame(in: .global).minX)").foregroundColor(.white)
+
+                                    ImageLoadingView(url: agent.fullPortrait!)
+                                }
+                                
+                            }.cornerRadius(20)
+                                .rotation3DEffect(Angle(degrees:(Double(geometry.frame(in: .global).minX) - HPadding) / -12), axis: (x: 0, y: 1.0, z: 0))
+                        }.frame(width: cardWidth)
+                        
+                    }
                 }
-            }.padding(.vertical, UIScreen.screenHeight * 0.03).padding(.horizontal, UIScreen.screenWidth  / 3.6)
-        }
+            }.frame(maxHeight: .infinity).padding(.vertical, UIScreen.screenHeight * 0.05).padding(.horizontal, HPadding)
+        }.frame(width: UIScreen.screenWidth)
     }
 }
 
--- Valorant Agents/Views/HomeView.swift
+++ Valorant Agents/Views/HomeView.swift
@@ -24,24 +24,25 @@ struct HomeView: View {
             
             VStack (alignment: .center){
                 // Logo
-                Image("logo").resizable().aspectRatio(contentMode: .fit).frame(width: UIScreen.screenWidth / 2.6).padding(.bottom, 5).padding(.top, UIScreen.screenHeight * 0.05)
+                Image("logo").resizable().aspectRatio(contentMode: .fit).frame(width: UIScreen.screenWidth / 2.5).padding(.bottom, 5).padding(.top, UIScreen.screenHeight * 0.05)
                 
                 // Title
-                Text("Choose Your").font(Font.custom("Valorant", size: 23)).foregroundColor(.white)
+                Text("Choose Your").font(Font.custom("Valorant", size: 25)).foregroundColor(.white)
                 HStack{
-                    Text("Favourite").font(Font.custom("Valorant", size: 23)).foregroundColor(.white)
-                    Text("Agents").font(Font.custom("Valorant", size: 23)).foregroundColor(valColor)
+                    Text("Favourite").font(Font.custom("Valorant", size: 25)).foregroundColor(.white)
+                    Text("Agents").font(Font.custom("Valorant", size: 25)).foregroundColor(valColor)
                 }
                 
-                CustomTopTabBar(tabIndex: $tabIndex).padding(.top, UIScreen.screenHeight * 0.05)
-                            
+                CustomTopTabBar(tabIndex: $tabIndex).padding(.top, UIScreen.screenHeight * 0.06)
+                
                 AgentsView().environmentObject(valAgents).onAppear {
                     // load Agents from Valorant Api
                     valAgents.getAgents()
                     
                     // Put Cache capacity to half GB
                     URLCache.shared.memoryCapacity = 1024 * 1024 * 512
-                }.frame(height: UIScreen.screenHeight  / 1.6)
+                }.frame(height: UIScreen.screenHeight  / 1.7
+                )
             }
             
         }
